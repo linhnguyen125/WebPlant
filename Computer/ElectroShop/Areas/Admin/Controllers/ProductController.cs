ecdc8b9a0176d90c213d0d0e744741e9a91fb651
linhnguyen125 <linhnguyennd125@gmail.com>
2021-09-06 22:09:25 +0700

    first commit

---
+++ Computer/ElectroShop/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ElectroShop.Models;
+
+namespace ElectroShop.Areas.Admin.Controllers
+{
+    public class ProductController : BaseController
+    {
+        private ElectroShopDbContext db = new ElectroShopDbContext();
+
+        // GET: Admin/Product
+        public ActionResult Index()
+        {
+            ViewBag.countTrash = db.Products.Where(m => m.Status == 0).Count();
+            var list = from p in db.Products
+                       join c in db.Categorys
+                       on p.CateID equals c.Id
+                       where p.Status != 0
+                       where p.CateID == c.Id
+                       orderby p.Created_at descending
+                       select new ProductCategory()
+                       {
+                           ProductId = p.ID,
+                           ProductImg = p.Image,
+                           ProductName = p.Name,
+                           ProductStatus = p.Status,
+                           ProductDiscount = p.Discount,
+                           ProductPrice = p.Price,
+                           ProductPriceSale = p.ProPrice,
+                           ProductCreated_At = p.Created_at,
+                           CategoryName = c.Name
+                       };
+            return View(list.ToList());
+        }
+        public ActionResult Trash()
+        {
+            var list = from p in db.Products
+                       join c in db.Categorys
+                       on p.CateID equals c.Id
+                       where p.Status == 0
+                       where p.CateID == c.Id
+                       orderby p.Created_at descending
+                       select new ProductCategory()
+                       {
+                           ProductId = p.ID,
+                           ProductImg = p.Image,
+                           ProductName = p.Name,
+                           ProductStatus = p.Status,
+                           ProductDiscount = p.Discount,
+                           ProductPrice = p.Price,
+                           ProductPriceSale = p.ProPrice,
+                           ProductCreated_At = p.Created_at,
+                           CategoryName = c.Name
+                       };
+            return View(list.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                Notification.set_flash("Không tồn tại!", "warning");
+                return RedirectToAction("Index");
+            }
+            MProduct mProduct = db.Products.Find(id);
+            if (mProduct == null)
+            {
+                Notification.set_flash("Không tồn tại!", "warning");
+                return RedirectToAction("Index");
+            }
+            return View(mProduct);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.countTrash = db.Products.Where(m => m.Status == 0).Count();
+            MCategory mCategory = new MCategory();
+            ViewBag.ListCat = new SelectList(db.Categorys.Where(m => m.Status != 0), "ID", "Name", 0);
+            //ViewBag.ListCat = new SelectList(db.Category.ToList(), "ID", "Name", 0);
+            return View();
+        }
+
+        [HttpPost, ValidateInput(false)]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(MProduct mProduct)
+        {
+            ViewBag.ListCat = new SelectList(db.Categorys.Where(m => m.Status != 0), "ID", "Name", 0);
+            if (ModelState.IsValid)
+            {
+                mProduct.Price = mProduct.Price;
+                mProduct.ProPrice = mProduct.ProPrice;
+
+                String strSlug = MyString.ToAscii(mProduct.Name);
+                mProduct.Slug = strSlug;
+                mProduct.Created_at = DateTime.Now;
+                mProduct.Created_by = int.Parse(Session["Admin_ID"].ToString());
+                mProduct.Updated_at = DateTime.Now;
+                mProduct.Updated_by = int.Parse(Session["Admin_ID"].ToString());
+
+                // Upload file
+                var file = Request.Files["Image"];
+                if (file != null && file.ContentLength > 0)
+                {
+                    String filename = strSlug + file.FileName.Substring(file.FileName.LastIndexOf("."));
+                    mProduct.Image = filename;
+                    String Strpath = Path.Combine(Server.MapPath("~/Public/library/product/"), filename);
+                    file.SaveAs(Strpath);
+                }
+
+                db.Products.Add(mProduct);
+                db.SaveChanges();
+                Notification.set_flash("Thêm mới sản phẩm thành công!", "success");
+                return RedirectToAction("Index");
+            }
+            return View(mProduct);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            ViewBag.countTrash = db.Products.Where(m => m.Status == 0).Count();
+            ViewBag.ListCat = new SelectList(db.Categorys.ToList(), "ID", "Name", 0);
+            MProduct mProduct = db.Products.Find(id);
+            if (mProduct == null)
+            {
+                Notification.set_flash("404!", "warning");
+                return RedirectToAction("Index", "Product");
+            }
+            return View(mProduct);
+        }
+
+        [HttpPost, ValidateInput(false)]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(MProduct mProduct)
+        {
+            ViewBag.ListCat = new SelectList(db.Categorys.ToList(), "ID", "Name", 0);
+            if (ModelState.IsValid)
+            {
+                String strSlug = MyString.ToAscii(mProduct.Name);
+                mProduct.Slug = strSlug;
+
+                mProduct.Updated_at = DateTime.Now;
+                mProduct.Updated_by = int.Parse(Session["Admin_ID"].ToString());
+
+                // Upload file
+                var file = Request.Files["Image"];
+                if (file != null && file.ContentLength > 0)
+                {
+                    String filename = strSlug + file.FileName.Substring(file.FileName.LastIndexOf("."));
+                    mProduct.Image = filename;
+                    String Strpath = Path.Combine(Server.MapPath("~/Public/library/product/"), filename);
+                    file.SaveAs(Strpath);
+                }
+
+                db.Entry(mProduct).State = EntityState.Modified;
+                db.SaveChanges();
+                Notification.set_flash("Đã cập nhật lại thông tin sản phẩm!", "success");
+                return RedirectToAction("Index");
+            }
+            return View(mProduct);
+        }
+
+        public ActionResult DelTrash(int? id)
+        {
+            MProduct mProduct = db.Products.Find(id);
+            mProduct.Status = 0;
+
+            mProduct.Updated_at = DateTime.Now;
+            mProduct.Updated_by = int.Parse(Session["Admin_ID"].ToString());
+            db.Entry(mProduct).State = EntityState.Modified;
+            db.SaveChanges();
+            Notification.set_flash("Ném thành công vào thùng rác!" + " ID = " + id, "success");
+            return RedirectToAction("Index");
+        }
+        public ActionResult Undo(int? id)
+        {
+            MProduct mProduct = db.Products.Find(id);
+            mProduct.Status = 2;
+
+            mProduct.Updated_at = DateTime.Now;
+            mProduct.Updated_by = int.Parse(Session["Admin_ID"].ToString()); ;
+            db.Entry(mProduct).State = EntityState.Modified;
+            db.SaveChanges();
+            Notification.set_flash("Khôi phục thành công!" + " ID = " + id, "success");
+            return RedirectToAction("Trash");
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                Notification.set_flash("Không tồn tại !", "warning");
+                return RedirectToAction("Trash");
+            }
+            MProduct mProduct = db.Products.Find(id);
+            if (mProduct == null)
+            {
+                Notification.set_flash("Không tồn tại !", "warning");
+                return RedirectToAction("Trash");
+            }
+            return View(mProduct);
+        }
+
+        // POST: Admin/Product/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MProduct mProduct = db.Products.Find(id);
+            db.Products.Remove(mProduct);
+            db.SaveChanges();
+            Notification.set_flash("Đã xóa vĩnh viễn sản phẩm!", "danger");
+            return RedirectToAction("Trash");
+        }
+
+        [HttpPost]
+        public JsonResult changeStatus(int id)
+        {
+            MProduct mProduct = db.Products.Find(id);
+            mProduct.Status = (mProduct.Status == 1) ? 2 : 1;
+
+            mProduct.Updated_at = DateTime.Now;
+            mProduct.Updated_by = int.Parse(Session["Admin_ID"].ToString());
+            db.Entry(mProduct).State = EntityState.Modified;
+            db.SaveChanges();
+            return Json(new { Status = mProduct.Status });
+        }
+        [HttpPost]
+        public JsonResult changeDiscount(int id)
+        {
+            MProduct mProduct = db.Products.Find(id);
+            mProduct.Discount = (mProduct.Discount == 1) ? 2 : 1;
+
+            mProduct.Updated_at = DateTime.Now;
+            mProduct.Updated_by = int.Parse(Session["Admin_ID"].ToString());
+            db.Entry(mProduct).State = EntityState.Modified;
+            db.SaveChanges();
+
+            return Json(new { Discount = mProduct.Discount });
+        }
+
+        //public JsonResult SendRating(string r, string s, string id, string url)
+        //{
+        //    int autoId = 0;
+        //    Int16 thisVote = 0;
+        //    Int16 sectionId = 0;
+        //    Int16.TryParse(s, out sectionId);
+        //    Int16.TryParse(r, out thisVote);
+        //    int.TryParse(id, out autoId);
+
+        //    if (!User.Identity.IsAuthenticated)
+        //    {
+        //        return Json("Not authenticated!");
+        //    }
+
+        //    if (autoId.Equals(0))
+        //    {
+        //        return Json("Sorry, record to vote doesn't exists");
+        //    }
+
+        //    switch (s)
+        //    {
+        //        case "5": // school voting
+        //            // check if he has already voted
+        //            var isIt = db.VoteModels.Where(v => v.SectionId == sectionId &&
+        //                v.UserName.Equals(User.Identity.Name, StringComparison.CurrentCultureIgnoreCase) && v.VoteForId == autoId).FirstOrDefault();
+        //            if (isIt != null)
+        //            {
+        //                // keep the school voting flag to stop voting by this member
+        //                HttpCookie cookie = new HttpCookie(url, "true");
+        //                Response.Cookies.Add(cookie);
+        //                return Json("<br />You have already rated this post, thanks !");
+        //            }
+
+        //            var sch = db.SchoolModels.Where(sc => sc.AutoId == autoId).FirstOrDefault();
+        //            if (sch != null)
+        //            {
+        //                object obj = sch.Votes;
+
+        //                string updatedVotes = string.Empty;
+        //                string[] votes = null;
+        //                if (obj != null && obj.ToString().Length > 0)
+        //                {
+        //                    string currentVotes = obj.ToString(); // votes pattern will be 0,0,0,0,0
+        //                    votes = currentVotes.Split(',');
+        //                    // if proper vote data is there in the database
+        //                    if (votes.Length.Equals(5))
+        //                    {
+        //                        // get the current number of vote count of the selected vote, always say -1 than the current vote in the array 
+        //                        int currentNumberOfVote = int.Parse(votes[thisVote - 1]);
+        //                        // increase 1 for this vote
+        //                        currentNumberOfVote++;
+        //                        // set the updated value into the selected votes
+        //                        votes[thisVote - 1] = currentNumberOfVote.ToString();
+        //                    }
+        //                    else
+        //                    {
+        //                        votes = new string[] { "0", "0", "0", "0", "0" };
+        //                        votes[thisVote - 1] = "1";
+        //                    }
+        //                }
+        //                else
+        //                {
+        //                    votes = new string[] { "0", "0", "0", "0", "0" };
+        //                    votes[thisVote - 1] = "1";
+        //                }
+
+        //                // concatenate all arrays now
+        //                foreach (string ss in votes)
+        //                {
+        //                    updatedVotes += ss + ",";
+        //                }
+        //                updatedVotes = updatedVotes.Substring(0, updatedVotes.Length - 1);
+
+        //                db.Entry(sch).State = EntityState.Modified;
+        //                sch.Votes = updatedVotes;
+        //                db.SaveChanges();
+
+        //                VoteModel vm = new VoteModel()
+        //                {
+        //                    Active = true,
+        //                    SectionId = Int16.Parse(s),
+        //                    UserName = User.Identity.Name,
+        //                    Vote = thisVote,
+        //                    VoteForId = autoId
+        //                };
+
+        //                db.VoteModels.Add(vm);
+
+        //                db.SaveChanges();
+
+        //                // keep the school voting flag to stop voting by this member
+        //                HttpCookie cookie = new HttpCookie(url, "true");
+        //                Response.Cookies.Add(cookie);
+        //            }
+        //            break;
+        //        default:
+        //            break;
+        //    }
+        //    return Json("<br />You rated " + r + " star(s), thanks !");
+        //}
+    }
+}
